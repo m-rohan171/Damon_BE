9cb63bc7d6106c2062bb25fa2826a7c993f7829f
kaami419 <tareen544@gmail.com>
2024-06-24 14:40:48 +0500

    latest-be

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const mongoose = require("mongoose");
 const dbConfig = require("./config/dbConfig");
 const routes = require("./routes");
 const path = require("path");
-const port = 5000;
+require("dotenv").config();
+const port = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
@@ -25,7 +26,7 @@ mongoose
   .then(() => {
     console.log("Connected to MongoDB");
     app.listen(port, "0.0.0.0", () => {
-      console.log("Server is running on port 5000");
+      console.log(`Server is running on port ${port}`);
     });
   })
   .catch((err) => {
